1cc2d310f2b6b03df0fd3101eec78463d3c27422
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-02-10 13:21:46 +0800

    class & interface basis data ditambahkan

---
+++ database/Database.php
@@ -0,0 +1,54 @@
+<?php
+
+// use database\IDatabase;
+namespace interfaces;
+
+interface IDatabase
+{
+    public function __construct($configuration = null);
+    public function connect();
+}
+
+namespace database;
+
+use interfaces\IDatabase;
+
+class Database implements IDatabase
+{
+    private $configuration;
+
+    /**
+     * Fungsi ini digunakan untuk menginstansiasikan + memberi konfigurasi khusus terhadap basis data.
+     *
+     * @param object|null $configuration
+     * @return null;
+     */
+    public function __construct($configuration = null)
+    {
+        if ($configuration == null) {
+            $configuration = (object) [
+                "serverName" => "localhost",
+                "name" => "root",
+                "password" => "",
+                "database" => "msdm_db"
+            ];
+        }
+
+        $this->configuration = $configuration;
+    }
+
+    /**
+     * Fungsi ini digunakan untuk menginstansiasikan + memberi konfigurasi khusus terhadap basis data.
+     *
+     * @return int|object;
+     */
+    public function connect()
+    {
+        $connection = new \mysqli($this->configuration->serverName, $this->configuration->name, $this->configuration->password, $this->configuration->database);
+        if ($connection->connect_errno) {
+            return 500;
+        } else {
+            return $connection;
+        }
+    }
+}
