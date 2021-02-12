5a874530898ce08dacb83009fd13be1082459a7a
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-02-12 10:20:13 +0800

    added base Department Repository & Service Interface

---
+++ lib/department/interfaces/IDepartmentRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace lib\department\interfaces;
+
+interface IDepartmentRepository
+{
+    public function store(IDepartment $department);
+    public function update(IDepartment $department);
+    /**
+     * @return IDepartment
+     */
+    public function findById(int $id);
+    public function delete(IDepartment $department);
+}
