c77a555e11d773703d3f9989043814081308e633
Putu Audi Pasuatmadi <63685606+audipasuatmadi@users.noreply.github.com>
2021-04-18 15:05:27 +0800

    Add/Investor (#10)
    * fix LIKe commit
    
    * fix findByRoles query
    
    * Merge commit 'e8a14285658f1ed0efd2035c224d799740f26097'
    
    * add/investor repository
    
    * bugfix/investor prepare statement
    
    * bugfix/investor prepare statement
    
    * add/investor service
    
    * add/final investor controller

---
+++ lib/investor/InvestorRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace lib\investor;
+
+use interfaces\IDatabase;
+use lib\investor\interfaces\IInvestorRepository;
+
+class InvestorRepository implements IInvestorRepository
+{
+    private IDatabase $database;
+
+    public function __construct(IDatabase $database)
+    {
+        $this->database = $database;
+    }
+
+    public function store($name, $stocks)
+    {
+        $conn = $this->database->connect();
+        $stmt = $conn->prepare(
+            "INSERT INTO investor (nama, jml_saham)
+            VALUES (?, ?)"
+        );
+        $stmt->bind_param("si", $name, $stocks);
+        $execResult = $stmt->execute();
+        if ($execResult == 1) {
+            $conn->close();
+            return ["status" => 201];
+        }
+        $conn->close();
+        return ["status" => 500];
+    }
+
+    public function getAll()
+    {
+        $conn = $this->database->connect();
+        $stmt = $conn->prepare(
+            "SELECT * FROM investor"
+        );
+        $execResult = $stmt->execute();
+        if ($execResult == 1) {
+            $results = $stmt->get_result();
+            if ($results->num_rows > 0) {
+                $allInvestor = $results->fetch_all(MYSQLI_ASSOC);
+                $conn->close();
+                return ["status" => 200, "payload" => $allInvestor];
+            }
+            $conn->close();
+            return ["status" => 404];
+        }
+        $conn->close();
+        return ["status" => 500];
+    }
+
+    public function update($id, $name, $stocks)
+    {
+        $conn = $this->database->connect();
+
+        $stmt = $conn->prepare(
+            "UPDATE investor
+            SET nama=?,
+            jml_saham=?
+            WHERE id=?"
+        );
+        $stmt->bind_param("sii", $name, $stocks, $id);
+
+        $execResult = $stmt->execute();
+        if ($execResult == 1) {
+            $conn->close();
+            return ["status" => 200];
+        }
+        $conn->close();
+        return ["status" => 500];
+    }
+
+    public function delete($id)
+    {
+        $conn = $this->database->connect();
+
+        $stmt = $conn->prepare(
+            "DELETE FROM investor WHERE id=?"
+        );
+        $stmt->bind_param("i", $id);
+
+        $execResult = $stmt->execute();
+        if ($execResult == 1) {
+            $conn->close();
+            return ["status" => 200];
+        }
+        $conn->close();
+        return ["status" => 500];
+    }
+}
---
+++ lib/investor/interfaces/IInvestorRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace lib\investor\interfaces;
+
+interface IInvestorRepository {
+    public function store($name, $stocks);
+    public function getAll();
+    public function update($id, $name, $stocks);
+    public function delete($id);
+}
